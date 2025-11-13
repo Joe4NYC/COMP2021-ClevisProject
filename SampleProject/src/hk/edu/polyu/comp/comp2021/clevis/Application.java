b7774ec045cc2c56c6d5504f529666fed662948d
Joe Ng <80151682+NYCHk80160@users.noreply.github.com>
2025-11-13 22:54:04 +0800

    Refactor Clevis and Application structure
    Removed STRUCTURE_CHECKLIST.md and ShapeManager copy.java. Simplified Application.java to directly instantiate Clevis and removed command-line argument parsing and initialization logic. Updated Clevis.java to instantiate shapes directly instead of delegating to ShapeManager for creation, and refactored group method to use shapeManager.group. Other command methods remain as placeholders for future implementation.

--- SampleProject/src/hk/edu/polyu/comp/comp2021/clevis/Application.java
+++ SampleProject/src/hk/edu/polyu/comp/comp2021/clevis/Application.java
@@ -1,31 +1,13 @@
 package hk.edu.polyu.comp.comp2021.clevis;
 
+import hk.edu.polyu.comp.comp2021.clevis.model.Clevis;
+
 public class Application {
 
-    private Clevis clevis;
-    private String htmlLogPath;
-    private String txtLogPath;
-
     public static void main(String[] args){
-        Application app = new Application();
-        app.initialize(args);
-        app.run();
-    }
-
-    // 初始化應用程序
-    private void initialize(String[] args) {
-        parseCommandLineArgs(args);
-        clevis = new Clevis(htmlLogPath, txtLogPath);
-    }
-    
-    // 解析命令行參數 (REQ1: -html and -txt parameters)
-    private void parseCommandLineArgs(String[] args) {
-        // TODO: 解析 -html 和 -txt 參數
-    }
-    
-    // 主程式迴圈
-    public void run() {
-        // TODO: 實現主運行邏輯
+        Clevis clevis = new Clevis();
+        // Initialize and utilize the system
+		
     }
 
 }
--- SampleProject/src/hk/edu/polyu/comp/comp2021/clevis/Clevis.java
+++ SampleProject/src/hk/edu/polyu/comp/comp2021/clevis/Clevis.java
@@ -16,17 +16,14 @@ public class Clevis {
     }
     
     public Clevis(String htmlLogPath, String txtLogPath) {
-        // TODO: 使用日誌路徑初始化 (REQ1)
         this.shapeManager = new ShapeManager();
         this.logger = new Logger(htmlLogPath, txtLogPath);
         this.operationCounter = 0;
     }
     
-    // REQ2: 創建矩形
     public void rectangle(String name, double x, double y, double w, double h) {
-        // TODO: 實現
         try {
-            Rectangle r = new Rectangle(name, x, y, h, h);
+            Rectangle rect = new Rectangle(name, x, y, h, h);
             if (logger != null) {
                 operationCounter ++;
                 logger.logCommand(operationCounter, "rectangle " + name + " " + x + " " + y + " " + w + " " + h);
@@ -37,11 +34,9 @@ public class Clevis {
         
     }
     
-    // REQ3: 創建線段
     public void line(String name, double x1, double y1, double x2, double y2) {
-        // TODO: 實現
         try {
-            shapeManager.createLine(name, x1, y1, x2, y2);
+            Line line = new Line(name, x1, y1, x2, y2);
             if (logger != null) {
                 operationCounter ++;
                 logger.logCommand(operationCounter, "line " + name + " " + x1 + " " + y1 + " " + x2 + " " + y2);
@@ -52,11 +47,9 @@ public class Clevis {
         }
     }
     
-    // REQ4: 創建圓形
     public void circle(String name, double x, double y, double r) {
-        // TODO: 實現
         try {
-            shapeManager.createCircle(name, x, y, r);
+            Circle circle = new Circle(name, x, y, r);
             if (logger != null) {
                 operationCounter ++;
                 logger.logCommand(operationCounter,"circle " + name + " " + x + " " + y + " " + r);
@@ -67,11 +60,9 @@ public class Clevis {
         }
     }
     
-    // REQ5: 創建正方形
     public void square(String name, double x, double y, double l) {
-        // TODO: 實現
         try {
-            shapeManager.createSquare(name, x, y, l);
+            Square square = new Square(name, x, y, l);
             if (logger != null) {
                 operationCounter ++;
                 logger.logCommand(operationCounter,"square " + name + " " + x + " " + y + " " + l);
@@ -82,12 +73,10 @@ public class Clevis {
         }
     }
     
-    // REQ6: 群組操作
     public void group(String groupName, String... shapeNames) {
-        // TODO: 實現
         try {
             List<String> nameList = Arrays.asList(shapeNames);
-            shapeManager.groupShapes(groupName, nameList);
+            shapeManager.group(groupName, nameList);
 
             if (logger != null) {
                 operationCounter ++;
@@ -102,9 +91,7 @@ public class Clevis {
         }
     }
     
-    // REQ7: 解除群組
     public void ungroup(String groupName) {
-        // TODO: 實現
         try {
             shapeManager.ungroupShapes(groupName);
             if (logger != null) {
@@ -115,59 +102,48 @@ public class Clevis {
         }
     }
     
-    // REQ8: 刪除形狀
     public void delete(String name) {
-        // TODO: 實現
         
     }
     
-    // REQ9: 計算邊界框
     public void boundingbox(String name) {
-        // TODO: 實現並輸出 "x y w h"
         
     }
     
-    // REQ10: 移動形狀
     public void move(String name, double dx, double dy) {
-        // TODO: 實現
+
     }
     
-    // REQ11: 找到覆蓋某點的最上層形狀
     public void shapeAt(double x, double y) {
-        // TODO: 實現並返回形狀名稱
+
     }
     
-    // REQ12: 判斷兩個形狀是否相交
     public void intersect(String n1, String n2) {
-        // TODO: 實現並報告是否相交
+
     }
     
-    // REQ13: 列出形狀的基本信息
     public void list(String name) {
-        // TODO: 實現
+
     }
     
-    // REQ14: 列出所有形狀
     public void listAll() {
-        // TODO: 實現，按Z-order降序，使用縮進顯示群組關係
+
     }
     
-    // REQ15: 退出程序
     public void quit() {
-        // TODO: 實現退出邏輯
+
     }
     
     // BON2: 撤銷操作
     public void undo() {
-        // TODO: 實現撤銷
+
     }
     
     // BON2: 重做操作
     public void redo() {
-        // TODO: 實現重做
+
     }
     
-    // 獲取組件
     public ShapeManager getShapeManager() {
         return shapeManager;
     }
